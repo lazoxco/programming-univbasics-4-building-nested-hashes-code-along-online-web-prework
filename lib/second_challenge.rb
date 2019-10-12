24f60a837c32d400edbeba862862c9fdcd057608
Nory Lazo <nlazo@icloud.com>
2019-10-12 22:53:36 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,16 +2,16 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       #your key/value pairs here!
-      :patriarch => {}
-      :matriarch => {}
-      :helper_method => {}
+      :patriarch => {},
+      :matriarch => {},
+      :helper_method => {},
       :hero_friends => []
       }, 
     :capulet => {
       #your key/value pairs here!
-      :patriarch => {}
-      :matriarch => {}
-      :heroine => {}
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
       :heroine_friends = []
       }}
 
