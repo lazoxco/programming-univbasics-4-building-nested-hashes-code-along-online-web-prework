6ffda5f911309593ded16dd858cd513914f535e6
Nory Lazo <nlazo@icloud.com>
2019-10-12 22:55:47 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -4,7 +4,7 @@ def second_challenge
       #your key/value pairs here!
       :patriarch => {},
       :matriarch => {},
-      :helper_method => {},
+      :hero => {},
       :hero_friends => []
       }, 
     :capulet => {
