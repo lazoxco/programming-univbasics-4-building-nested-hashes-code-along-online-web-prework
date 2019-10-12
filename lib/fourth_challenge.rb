1d0c5927d6f2660ffc4cc1e7e3c0b6acf183d06b
Nory Lazo <nlazo@icloud.com>
2019-10-12 23:18:52 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -10,7 +10,7 @@ def fourth_challenge
           :name => "Benvolio",
           :age => "17",
           :attitude => "worried"
-        }
+        },
         {
           :name => "Mercutio",
           :age => "18",
@@ -24,6 +24,16 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         #your hashes here!
+        {
+          :name => "Steven",
+          :age => "30",
+          :attitude => "confused"
+        },
+        {
+          :name => "Nurse",
+          :age => "44",
+          :attitude => "worried"
+        }
       ]
    }
 }
